9f7c1b4775ec3e65b0d6965ea3c38e20b6992635
moono <toilety@gmail.com>
2018-11-26 15:09:06 +0900

    add experimental multi-gpu training

---
+++ train_classifier_mirrored.py
@@ -0,0 +1,93 @@
+import os
+import argparse
+import tensorflow as tf
+
+from utils.helpers import get_proper_fn
+from utils.dataset_loaders import load_dataset
+from utils.classifier_fns import input_fn, model_fn
+from utils.best_checkpoint_exporter import BestCheckpointExporter
+
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+# arguments parser
+parser = argparse.ArgumentParser(description='', allow_abbrev=False)
+parser.add_argument('--network_module', help='', default='resnet.network_resnet')
+parser.add_argument('--network_name', help='', default='resnet83')
+parser.add_argument('--dataset_name', help='', default='cifar10')
+parser.add_argument('--epochs', help='', default=10, type=int)
+parser.add_argument('--batch_size', help='', default=256, type=int)
+parser.add_argument('--learning_rate', help='', default=0.1, type=float)
+parser.add_argument('--weight_decay', help='', default=1e-4, type=float)
+args = vars(parser.parse_args())
+
+
+def best_exporter_compare_fn(best_eval_result, current_eval_result):
+    default_key = 'accuracy'
+    if not best_eval_result or default_key not in best_eval_result:
+        raise ValueError('best_eval_result cannot be empty or no mean_iou is found in it.')
+
+    if not current_eval_result or default_key not in current_eval_result:
+        raise ValueError('current_eval_result cannot be empty or no mean_iou is found in it.')
+
+    return best_eval_result[default_key] < current_eval_result[default_key]
+
+
+def train():
+    # parse arguments
+    network_module = args['network_module']
+    network_name = args['network_name']
+    dataset_name = args['dataset_name']
+    epochs = None if args['epochs'] == 0 else args['epochs']
+    batch_size = args['batch_size']
+    learning_rate = args['learning_rate']
+    weight_decay = args['weight_decay']
+
+    # get testing network
+    network_fn = get_proper_fn(network_module, network_name)
+
+    # set model_dir
+    model_dir = os.path.join('/tmp', 'cnn', dataset_name, network_name)
+
+    # grab data
+    trainset, testset, input_size, n_classes = load_dataset(dataset_name)
+
+    # create run config for estimator
+    distribution = tf.contrib.distribute.MirroredStrategy()
+    run_config = tf.estimator.RunConfig(keep_checkpoint_max=2,
+                                        save_checkpoints_steps=2000,
+                                        train_distribute=distribution)
+
+    # create the Estimator
+    model = tf.estimator.Estimator(
+        model_fn=model_fn,
+        model_dir=model_dir,
+        config=run_config,
+        params={
+            'network_fn': network_fn,
+            'n_classes': n_classes,
+            'weight_decay': weight_decay,
+            'learning_rate': learning_rate,
+        },
+        warm_start_from=None,
+    )
+
+    # set best model exporter
+    best_model_exporter = BestCheckpointExporter(compare_fn=best_exporter_compare_fn, num_to_keep=2)
+
+    # start training...
+    train_spec = tf.estimator.TrainSpec(
+        input_fn=lambda: input_fn(trainset['images'], trainset['labels'], input_size, epochs, batch_size, True),
+        max_steps=None,
+    )
+    eval_spec = tf.estimator.EvalSpec(
+        input_fn=lambda: input_fn(testset['images'], testset['labels'], input_size, 1, 100, False),
+        exporters=best_model_exporter,
+    )
+
+    tf.estimator.train_and_evaluate(model, train_spec, eval_spec)
+    return
+
+
+if __name__ == '__main__':
+    train()
